b134de2c3680001c7294889c9f9936f2d459b6bf
Xpiral21 <adry_is@yahoo.com>
2016-12-15 18:29:00 +0200

    plus

--- app/src/main/java/com/example/java2/googlemaps/MainActivity.java
+++ app/src/main/java/com/example/java2/googlemaps/MainActivity.java
@@ -25,6 +25,8 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     private final String TAG="tralala";
     private LocationManager lm;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
