c19b3faad328041c28e1223e10bd6708fa87eac6
Xpiral21 <adry_is@yahoo.com>
2016-12-15 18:22:10 +0200

    plus

--- app/src/main/java/com/example/java2/googlemaps/MainActivity.java
+++ app/src/main/java/com/example/java2/googlemaps/MainActivity.java
@@ -3,17 +3,27 @@ package com.example.java2.googlemaps;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity implements LocationListener {
-    private static final float MIN_DISTANCE_METERS = 5;
-    private static final long MIN_TIME_UPDATE = 10;
+    private static final float MIN_DISTANCE_METERS = 1;
+    private static final long MIN_TIME_UPDATE = 1;
+    private final String TAG="tralala";
+    private LocationManager lm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +33,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     }
 
     private void init() {
-        LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         boolean gps = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
         boolean net = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
@@ -35,14 +45,19 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
+
             return;
         }
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_UPDATE, MIN_DISTANCE_METERS, this);
+        checkBestProvider();
+        Location lastKnownLocation=lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        Log.v(TAG," "+lastKnownLocation+ "last known loc");
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_UPDATE, MIN_DISTANCE_METERS, this);
     }
 
     @Override
     public void onLocationChanged(Location location) {
-        Log.d("locationCHanged", location.toString());
+        Log.v("locationCHanged", location.toString());
+        getAdress(location.getLatitude(),location.getLongitude());
     }
 
     @Override
@@ -58,5 +73,31 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     @Override
     public void onProviderDisabled(String provider) {
         Log.d("onProviderDisabl", provider);
+    }
+
+    private void getAdress(double latitude,double longitude){
+        Geocoder geocoder =new Geocoder(getApplicationContext(), Locale.getDefault());
+        try{
+            List<Address> listAdresses=geocoder.getFromLocation(latitude,longitude,1);
+            if(null!=listAdresses&&listAdresses.size()>0){
+                String location=listAdresses.get(0).getAddressLine(0);
+                String countryCode=listAdresses.get(0).getCountryCode();
+                String countryName=listAdresses.get(0).getCountryName();
+                Log.v(TAG,"country code: "+countryCode+" country name: "+countryName+" location: "+location);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void checkBestProvider(){
+
+        Criteria criteria=new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setCostAllowed(true);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        String locationProvider=lm.getBestProvider(criteria,true);
+        Log.v(TAG," loc provider: "+ locationProvider);
     }
 }
